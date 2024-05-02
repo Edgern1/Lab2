faa0234ac2565b4aa25689e23ff4591e26411447
me <me@mes-MacBook-Pro.local>
2024-05-02 10:37:20 +0800

    addedded thingy to thingy for thingy to do things

--- bmi.py
+++ bmi.py
@@ -4,9 +4,11 @@ def calculate_bmi(height, weight):
 	bmi=weight/(height**2)
 	print("BMI = "+str(bmi))
 	if bmi <18.5:
-		print ("you are under weight")
+		#print ("you are under weight")
+		return -1
 	elif bmi > 25:
-		print("you are over weight")
+		#print("you are over weight")
+		return 0
 	else:
-		print("you are of normal weight")
-calculate_bmi(weight=57, height=1.73)
+		#print("you are of normal weight")
+		return 1
